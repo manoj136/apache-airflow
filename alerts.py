bef4479f8f0bd6bfc770da6d1a0d1bc65860bc30
manoj136 <126874614+manoj136@users.noreply.github.com>
2024-07-02 11:44:15 +0530

    Update alerts.py

--- alerts.py
+++ alerts.py
@@ -1,11 +1,14 @@
 from airflow import DAG 
 from airflow.operators.email import EmailOperator
 
+dag = DAG('send_alert_dag', schedule_interval='@daily')
+
 send_email = EmailOperator(
     task_id='send_email',
     to='mkumar@valethi.in',
     subject='Airflow Alert',
-    html_content='<p>Your Airflow job has finished.</p>'
+    html_content='<p>Your Airflow job has finished.</p>',
+    dag=dag
 )
 
 send_email
