c1ed1f4f18083e411a7dce9231d2855542a5122f
manoj136 <126874614+manoj136@users.noreply.github.com>
2024-07-02 11:56:13 +0530

    Update alerts.py

--- alerts.py
+++ alerts.py
@@ -11,6 +11,7 @@ dag = DAG('send_alert_dag', default_args=default_args, schedule_interval='@daily
 
 send_email = EmailOperator(
     task_id='send_email',
+    email_conn_id='SMTP_Connection',
     to='mkumar@valethi.in',
     subject='Airflow Alert',
     html_content='<p>Your Airflow job has finished.</p>',
